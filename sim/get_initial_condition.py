108627a34d2e7cb409cc38c9dd218cf571bd4ed3
Jo Bovy <jo.bovy@gmail.com>
2014-12-18 22:05:39 +0000

    ophiuchus: notes about how to setup a sensible N-body simulation

---
+++ sim/get_initial_condition.py
@@ -0,0 +1,33 @@
+import sys
+import numpy
+from galpy.potential import MWPotential2014
+from galpy.orbit import Orbit
+from galpy.util import bovy_conversion
+def get_initial_condition(to):
+    """Find an initial condition near apocenter that ends up today near the end of the stream and is close to a gyrfalcon stepsize
+    to= Find an apocenter that is just more recent than this time"""
+    vo, ro= 220., 8.
+    # Center of the stream
+    o= Orbit([0.10035165,-0.81302488,0.80986668,0.58024425,0.92753945,
+               0.88763126],ro=ro,vo=vo)
+    #Flip for backwards integration
+    of= o.flip()
+    ts= numpy.linspace(0.,to/bovy_conversion.time_in_Gyr(vo,ro),10001)
+    of.integrate(ts,MWPotential2014)
+    # Find the closest apocenter to the time to
+    rs= numpy.sqrt(of.R(ts)**2.+of.z(ts)**2.)
+    drs= rs-numpy.roll(rs,1)
+    nearApo= (drs < 0.)*(numpy.roll(drs,1) > 0.)
+    tsNearApo= ts[nearApo]
+    #Convert to gyrfalcon units
+    tsNearApo*= bovy_conversion.time_in_Gyr(vo,ro)/0.9777922212082034
+    tfgf= numpy.amax(tsNearApo)
+    #Round to nearest 0.00390625
+    tfgf= round(tfgf/0.00390625)*0.00390625
+    tf= tfgf/bovy_conversion.time_in_Gyr(vo,ro)*0.9777922212082034
+    print 'Current: %s,%s,%s,%s,%s,%s' % (of.x()[0], of.y()[0], of.z(), -of.vx()[0], -of.vy()[0], -of.vz())
+    print 'At %g Gyr (gyrfalcon %g kpc/(km/s))): %s,%s,%s,%s,%s,%s' % (tf*bovy_conversion.time_in_Gyr(vo,ro),tfgf,of.x(tf)[0], of.y(tf)[0], of.z(tf), -of.vx(tf)[0], -of.vy(tf)[0], -of.vz(tf))
+    return None
+
+if __name__ == '__main__':
+    get_initial_condition(float(sys.argv[1]))
